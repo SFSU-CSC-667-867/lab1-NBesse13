e784d7fd772cdbf0682f57ab1df827f887a44cb9
Nick Besse <nbesse@mail.sfsu.edu>
2019-02-06 20:28:13 -0800

    functions are wierd

--- node/functions.js
+++ node/functions.js
@@ -10,10 +10,14 @@ const arrow = (test) => {
   console.log(test);
 }
 
-arrow('hi')
+arrow('hi');
 
 // for practice
 
 // make an arrow function with no block body
-
+const myArrow = (y) => y + 1;
+console.log(myArrow(2));
 // make a function that returns a function
+const functionFactory = () => () => console.log('I said stop, damn it');
+const a = functionFactory();
+a();
